4b73496331eff50ea1dc0c9f550ac4e2c2ac2c45
bazilvaldez30 <bazilvaldez30@gmail.com>
2023-05-12 21:18:47 +0800

    fix error

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script> */}
       </body>
     </Html>
   )
