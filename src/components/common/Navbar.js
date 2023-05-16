733958c083243dec41926c5477bc0ff3f8e6696e
bazilvaldez30 <bazilvaldez30@gmail.com>
2023-05-16 20:37:01 +0800

    some changes

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -71,12 +71,9 @@ const Navbar = () => {
               <div className="flex space-x-4">
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 <Link href="/products" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Products</Link>
-
-                <Link href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
-
+                {/* <Link href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
                 <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
-
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a> */}
               </div>
             </div>
           </div>
